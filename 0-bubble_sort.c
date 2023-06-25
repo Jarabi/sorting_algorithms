ffad22af92644ec6c1d4197f8150c527fc651592
Alex Jarabi <jarabialex.gmail.com>
2023-06-25 16:33:52 -0700

    Fix
    Move print_array function inside condition when elements are swapped.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,13 +9,12 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t m, n;
-	int temp, swap;
+	int temp;
 
 	if (array != NULL && size > 1)
 	{
 		for (m = 0; m < size - 1; m++)
 		{
-			swap = 0;
 			for (n = 0; n < size - m - 1; n++)
 			{
 				if (array[n] > array[n + 1])
@@ -23,10 +22,8 @@ void bubble_sort(int *array, size_t size)
 					temp = array[n];
 					array[n] = array[n + 1];
 					array[n + 1] = temp;
-					swap = 1;
-				}
-				if (swap)
 					print_array(array, size);
+				}
 			}
 		}
 	}
